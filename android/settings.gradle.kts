a3d4a39cd300ab8e5d43b59545a882691021e27a
ado162 <andrej.galik.ag@gmail.com>
2026-06-11 22:29:58 +0200

    oprava prechodnych build gradle suborov

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -22,12 +22,12 @@ plugins {
     id("com.android.application") version "8.6.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
-/*dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
     }
-}*/
+}
 
 include(":app")
